0ab2265283f28745ad7f38bce8eab5b33ebcab2f
Anthial <sareizeer@gmail.com>
2024-08-28 22:37:42 +0200

    are we async now

--- src/module/item/stunt/StuntItem.ts
+++ src/module/item/stunt/StuntItem.ts
@@ -13,7 +13,7 @@ export class StuntItem extends BaseItem {
 
         for (const stunt of sheetData.stunts) {
             // @ts-ignore
-            stunt.system.description = TextEditor.enrichHTML(stunt.system.description, { async: false });
+            stunt.system.description = TextEditor.enrichHTML(stunt.system.description, { async: true });
         }
 
         return sheetData;
@@ -21,7 +21,7 @@ export class StuntItem extends BaseItem {
 
     static getSheetData(sheetData) {
         // @ts-ignore
-        sheetData.enrichedDescription = TextEditor.enrichHTML(sheetData.system.description, { async: false });
+        sheetData.enrichedDescription = TextEditor.enrichHTML(sheetData.system.description, { async: true });
     }
 
     static activateActorSheetListeners(html, sheet) {
